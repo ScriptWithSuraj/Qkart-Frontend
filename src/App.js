6ddc0544b85c3b956744d9b8fede499ea7d5f598
Suraj <coolsuraj96@gmail.com>
2023-01-10 22:53:17 +0530

    Changed the backend endpoint from heroku to render

--- src/App.js
+++ src/App.js
@@ -3,39 +3,39 @@ import ipConfig from "./ipConfig.json";
 import { Route, Switch } from "react-router-dom";
 import Login from "./components/Login";
 import Products from "./components/Products";
-import Checkout from "./components/Checkout"
-import Thanks from "./components/Thanks"
-import { ThemeProvider } from '@mui/system';
-import theme  from './theme';
-import React from 'react';
+import Checkout from "./components/Checkout";
+import Thanks from "./components/Thanks";
+import { ThemeProvider } from "@mui/system";
+import theme from "./theme";
+import React from "react";
 export const config = {
-  endpoint: `https://suraj-qkart-frontend.herokuapp.com/api/v1`,
+  endpoint: `https://qkart-frontend1-2ead.onrender.com/api/v1`,
 };
 
 function App() {
   return (
     <div className="App">
-    {/* <ThemeProvider theme={theme}> */}
+      {/* <ThemeProvider theme={theme}> */}
       {/* <BrowserRouter> */}
       <Switch>
-      <Route path="/register">
-          <Register/>
-          </Route>
-          <Route path="/login">
-          <Login/>
-         </Route>
-         <Route path="/checkout">
-            <Checkout/>
-          </Route>
-          <Route path="/thanks">
-            <Thanks/>
-          </Route>
-          <Route path="/">
-          <Products/>
-          </Route>
-          </Switch>
-          {/* </BrowserRouter> */}
-          {/* </ThemeProvider> */}
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/checkout">
+          <Checkout />
+        </Route>
+        <Route path="/thanks">
+          <Thanks />
+        </Route>
+        <Route path="/">
+          <Products />
+        </Route>
+      </Switch>
+      {/* </BrowserRouter> */}
+      {/* </ThemeProvider> */}
     </div>
   );
 }
